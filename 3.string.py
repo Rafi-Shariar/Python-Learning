c09a51fc98d74cf807c7ec7768d1b425ecc0ba70
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-01-17 20:08:10 +0600

    strings in python

---
+++ 3.string.py
@@ -0,0 +1,20 @@
+str1 = "this is string1"
+str2 = 'string2'
+str3 = """this is string 3"""
+
+# print(str2)
+# print(len(str1))
+# print(str2[3])
+
+print("Slice str2 : ", str2[2:5])
+
+# python supports -ev indexing. Example : Apple -> -5 -4 -3 -2 -1
+print("-ev Indexing slice: ", str2[-4:-1])
+
+#string functions
+print(str2.endswith("2"))
+print(str2.capitalize())
+print(str2.replace('i','X'))
+print(str2.find("s"))
+print(str3.count("i"))
+
