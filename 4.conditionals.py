3604d4a27d9e735a6baf5f7dab2c5b7fd6133177
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-01-17 20:14:59 +0600

    conditional statements in python

---
+++ 4.conditionals.py
@@ -0,0 +1,27 @@
+a = 10
+b = 20
+#if else
+if(a > b):
+    print("a is bigger")
+elif(a==b):
+    print("a is equal")
+else:
+    print("b is bigger")
+
+# nested if else
+x = "red"
+y = "green"
+color = x;
+if(color == x):
+    if(y == "green"):
+        print("Yes")
+    else:
+        print("No")
+else:
+    print("Invalid")
+
+# combinational if else
+if( a == 10 and b == 20):
+    print("OK")
+else:
+    print("Not OK")
