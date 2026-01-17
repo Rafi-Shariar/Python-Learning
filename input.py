e1f645c4dd52613795eed65b47bc56bfe627ef0d
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-01-17 19:25:59 +0600

    Inputs in python

---
+++ input.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print("Welcome ", name)
+
+
+"""
+in defult every input data in string. To take specific data type as input
+we need to cover it by that data type.
+"""
+Rollno = int(input("Enter your roll no : "))
+print("Roll no : ", Rollno)
---
+++ test.py
@@ -0,0 +1,3 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+print("Sum", num1 + num2)
