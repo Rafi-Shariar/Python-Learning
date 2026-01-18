c8b755b443b72574a6d2ed0438db800c7d4536c2
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-01-18 21:16:42 +0600

    list & tupple

---
+++ 5.list.py
@@ -0,0 +1,16 @@
+marks = [12, 1, 2, 3]
+print(marks)
+
+student = ["rafi", 12, "Sakib", 23, "eaqub", 500]
+print(student)
+# slice
+print( student[2:4])
+
+marks.append(99)
+print(marks)
+
+marks.sort()
+print(marks)
+
+marks.insert(2,999)
+print(marks)
---
+++ tupples.py
@@ -0,0 +1,5 @@
+tup = (1,2,3,4,5)
+# tupples are immutable but list is mutable
+# returns index of 1st occurance
+print(tup.index(4)) 
+
